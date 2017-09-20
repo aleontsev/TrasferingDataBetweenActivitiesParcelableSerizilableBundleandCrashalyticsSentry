e40576617891a05077848e95a34109d0b7ba33ca
Anton <aleontsev@gmail.com>
2017-09-20 12:03:09 +0300

    First commit

---
+++ app/src/main/java/service_test/service_test/MainActivity.java
@@ -0,0 +1,28 @@
+package service_test.service_test;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+    final String LOG_TAG = "myLogs";
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+    public void onClickStart(View v) {
+        startService(new Intent(this, MyService.class));
+    }
+
+    public void onClickStop(View v) {
+        stopService(new Intent(this, MyService.class));
+    }
+}
